25289428a2edff085a1dd094fe5e4987479661cc
Leo Žarković <lzarkov@racunarstvo.hr>
2022-06-20 23:06:40 +0200

    Fixed StageName

--- DataLayer/Managers/Manager.cs
+++ DataLayer/Managers/Manager.cs
@@ -12,8 +12,8 @@ namespace DataLayer.Managers
     public class Manager
     {
         private readonly IRepository repository;
-        //public Manager() => repository = RepositoryFactory.GetFileRepository();
-        public Manager() => repository = RepositoryFactory.GetAPIRepository();
+        public Manager() => repository = RepositoryFactory.GetFileRepository();
+        //public Manager() => repository = RepositoryFactory.GetAPIRepository();
 
         public Task<IList<TeamFromResults>> GetAllTeams() => repository.GetAllTeams();
         public Task<ISet<Player>> GetPlayers(string fifaCode) => repository.GetPlayers(fifaCode);
